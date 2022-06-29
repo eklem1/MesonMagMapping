d7e69bc3a8e01724b54e732d5b2f4c1a51703d73
eklem1 <eklemets@triumf.ca>
2022-06-29 16:06:18 -0700

    editing ploting function

--- PENTrackAnalysis/ParticlePlottingFunctions.py
+++ PENTrackAnalysis/ParticlePlottingFunctions.py
@@ -48,7 +48,10 @@ def pltShape(x, y, z, startPlot=True, endPlot=True, label="", figVals=None, ls="
     ax4.set_xlabel('y')
     ax4.set_ylabel('z')
     if endPlot:
-        ax0.legend()
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+        ax0.legend(by_label.values(), by_label.keys())
+#         ax0.legend()
         plt.show()
     else:
         #if it's not the end of the graph, return the figure objects
@@ -83,8 +86,91 @@ def plot3D(x, y, z, startPlot=True, endPlot=True, label="", figVals=None, ls="No
         return [fig, ax1]
     
     
+'''
+A function to graph the 3D positions of the UCNs, in a 3D scatter plot, with coloring of points
+as a function of the geometry they are in, and also the option to label each point with
+the timing [s].
+'''
+def plot3D_geoColor(x, y, z, geo, t=None, startPlot=True, endPlot=True, label="", \
+                    figVals=None, ls="None", text_times=False, size=(8,8)):
+    if startPlot:
+        fig = plt.figure(figsize=size)
+        ax1 = fig.add_subplot(1,1,1, projection='3d')
+        ax1.set_xlim3d(-5, 7)
+        ax1.set_ylim3d(-2, 0)
+        ax1.set_zlim3d(-0.6, 0.5)
+        ax1.set_xlabel('x')
+        ax1.set_ylabel('y')
+        ax1.set_zlabel('z')
+        
+    else:
+        #unpack the array that holds all the fig axes
+        fig, ax1 = figVals
+        
+    #get the label and matching color for the geometry type that each neutron is in
+    point_features = np.transpose([getColor(g) for g in geo] )
+    colors = point_features[0]
+    geo_label = point_features[1]
     
+    #for connections between points if wanted
+    ax1.plot3D(x, y, z, linestyle=ls, marker='') 
+    #for different colors of each point
+    ax1.scatter(x, y, z, c=colors)
     
+    #includes the labeling for each geometry color
+    for i, c in enumerate(colors):
+        ax1.plot3D(100, 100, 100, linestyle="", marker='o', c=c, label=geo_label[i])
+    
+    if text_times:
+        for i in range(len(x)): #plot each point's time as text above
+            ax1.text(x[i],y[i],z[i],  '%s' % (str(t[i])), size=15, zorder=1, color='k') 
+    
+    #if it's the end of the plot, call the legend and show it
+    if endPlot:
+        #removes repeated labels so this doesn't look silly
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+        ax1.legend(by_label.values(), by_label.keys())
+
+#         ax1.legend()
+        plt.show()
+        #could add option to save the graph too here
+    else:
+        #if it's not the end of the graph, return the figure objects
+        return [fig, ax1]
+'''
+returns the coorsponding geometry label and set color for a particular PENTrack
+geometry tag.
+Default for unknown/not yet defined geometry parts is black.
+'''
+def getColor(geo1):
+    color = 'black'
+    label = None
+
+    if geo1 == 183:
+        #Long Y guide
+        color = 'red'
+        label = 'Y_guide'
+    elif geo1 in [187, 188]:
+        #guides into chambers
+        color = 'red'
+    elif (2 <= geo1 <= 33) | (37 <= geo1 <= 114):
+        #He vapour and liquid volumes 
+        color='blue'
+        label = 'He'
+    elif geo1 in [185, 186]:
+        #in the chambers to do the rest of the experiment
+        color = 'black'
+        label = 'in cell'
+    elif geo1 == 1:
+        #the default volume geometry
+        color = 'orange'
+        label = '1'
+        
+    else: #to catch new geometry labels coming up
+        print(geo1)
+        
+    return color, label
     
     
 '''
